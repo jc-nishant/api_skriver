2a931deb5abc3ae7fdd92bb02b11ba6ebd5bd358
Shiv <shivmehta108@gmail.com.com>
2023-07-28 18:26:34 +0530

    category listing updated

--- api/controllers/CategoryController.js
+++ api/controllers/CategoryController.js
@@ -7,312 +7,323 @@
 
 var constantObj = sails.config.constants;
 const db = sails.getDatastore().manager;
+// console.log(db, "database");
 
 module.exports = {
-
   /*For Add category*/
 
   addCategory: async (req, res) => {
+    try {
+      const data = req.body;
 
-      try {
-          const data = req.body
-
-          if (!data.name || typeof data.name === undefined) {
-              return res.status(404).json({
-                  success: false,
-                  error: { code: 404, message: constantObj.CATEGORY.NAME_REQUIRED }
-              })
-          }
-
-          let existed = await Category.findOne({ name: data.name, isDeleted: false });
-          if (existed) {
-              return res.status(400).json({
-                  "success": false,
-                  error: {"code": 400,"message": constantObj.CATEGORY.NAME_EXIST},
-              });
-          } else {
-              data.addedBy = req.identity.id;
-
-
-              let items = await Category.create(data).fetch();
-              if (items) {
-                  return res.status(200).json({
-                      "success": true,
-                      "code": 200,
-                      "data": items,
-                      "message": constantObj.CATEGORY.ADDED,
-                  });
-              };
-          }
-      } catch (err) {
-          console.log(err);
-          return res.status(400).json({ success: false, code: 400, error: err });
+      if (!data.name || typeof data.name === undefined) {
+        return res.status(404).json({
+          success: false,
+          error: { code: 404, message: constantObj.CATEGORY.NAME_REQUIRED },
+        });
       }
+
+      let existed = await Category.findOne({
+        name: data.name,
+        isDeleted: false,
+      });
+      if (existed) {
+        return res.status(400).json({
+          success: false,
+          error: { code: 400, message: constantObj.CATEGORY.NAME_EXIST },
+        });
+      } else {
+        data.addedBy = req.identity.id;
+
+        let items = await Category.create(data).fetch();
+        if (items) {
+          return res.status(200).json({
+            success: true,
+            code: 200,
+            data: items,
+            message: constantObj.CATEGORY.ADDED,
+          });
+        }
+      }
+    } catch (err) {
+      console.log(err);
+      return res.status(400).json({ success: false, code: 400, error: err });
+    }
   },
 
   /*For list category*/
 
-//   listCategory: async (req, res) => {
-//       try {
+  listCategory: async (req, res) => {
+    try {
+      console.log("here");
+      var search = req.param("search");
+      var isDeleted = req.param("isDeleted");
+      var page = req.param("page");
+      var type = req.param("type");
+      var categoryId = req.param("categoryId");
+      var cat_type = req.param("cat_type");
+      var sortBy = req.param("sortBy");
+      let status = req.param("status");
+      let language = req.param("language");
+      let Parent_category_id = req.param("Parent_category_id");
 
-//           var search = req.param('search');
-//           var isDeleted = req.param('isDeleted');
-//           var page = req.param('page');
-//           var type = req.param('type');
-//           var categoryId = req.param('categoryId');
-//           var cat_type = req.param('cat_type');
-//           var sortBy = req.param('sortBy');
-//           let status = req.param('status')
-//           let language = req.param("language")
-//           let Parent_category_id = req.param("Parent_category_id")
+      if (!page) {
+        page = 1;
+      }
+      var count = parseInt(req.param("count"));
+      if (!count) {
+        count = 10000;
+      }
+      var skipNo = (page - 1) * count;
+      var query = {};
 
-//           if (!page) {
-//               page = 1
-//           }
-//           var count = parseInt(req.param('count'));
-//           if (!count) {
-//               count = 10000
-//           }
-//           var skipNo = (page - 1) * count;
-//           var query = {};
+      if (Parent_category_id) {
+        query.Parent_category_id = ObjectId(Parent_category_id);
+      }
+      if (type) {
+        query.Parent_category_id = null;
+      }
+      // console.log(sub_category,"======================sub_category")
+      // console.log(query,"========================================query")
 
+      if (search) {
+        query.$or = [{ name: { $regex: search, $options: "i" } }];
+      }
 
-//           if (Parent_category_id) {
-//               query.Parent_category_id = ObjectId(Parent_category_id);
-//           }
-//           if(type){
-//               query.Parent_category_id=null
-//           }
-//           // console.log(sub_category,"======================sub_category")
-//           // console.log(query,"========================================query")
+      query.isDeleted = false;
 
-//           if (search) {
-//               query.$or = [
-//                   { name: { $regex: search, '$options': 'i' } }
-//               ]
-//           }
+      if (cat_type) {
+        query.cat_type_id = ObjectId(cat_type);
+      }
+      console.log(cat_type);
+      // if(Parent_category_id==null||!Parent_category_id){
 
-//           query.isDeleted = false
+      // }
 
-//           if (cat_type) {
-//               query.cat_type_id = ObjectId(cat_type);
-//           }
-//           console.log(cat_type)
-//           // if(Parent_category_id==null||!Parent_category_id){
+      sortquery = {};
+      if (sortBy) {
+        var order = sortBy.split(" ");
+        var field = order[0];
+        var sortType = order[1];
+      }
 
-//           // }
+      sortquery[field ? field : "updatedAt"] = sortType
+        ? sortType == "descending"
+          ? -1
+          : 1
+        : -1;
+      // console.log(sortquery, "===================================")
+      if (status) {
+        query.status = status;
+      }
 
-//           sortquery = {};
-//           if (sortBy) {
-//             var order = sortBy.split(" ");
-//             var field = order[0];
-//             var sortType = order[1];
-//           }
-    
-//           sortquery[field ? field : 'updatedAt'] = sortType ? (sortType == 'descending' ? -1 : 1) : -1;
-//           // console.log(sortquery, "===================================")
-//           if (status) {
-//               query.status = status
-//           }
+      console.log(query);
+      const allCategories = await Category.find(query).sort(sortBy).limit(count).skip((page - 1) * count);
+      if(allCategories){
+        return res.status(200).json({
+            success: true,
+            message: constantObj.CATEGORY.ALL_DATA,
+            data: allCategories
+        })
+      } else {
+        return res.status(400).json({
+            success: false,
+            message: constantObj.CATEGORY.FAILED
+        })
+    }
+    //   db.collection("category")
+    //     .aggregate([
+    //       {
+    //         $project: {
+    //           id: "$_id",
+    //           isDeleted: "$isDeleted",
+    //           name: "$name",
+    //           description: "$description",
+    //           cat_type: "$cat_type",
+    //           image: "$image",
+    //           status: "$status",
+    //           createdAt: "$createdAt",
+    //           deletedBy: "$deletedBy.fullName",
+    //           deletedAt: "$deletedAt",
+    //           addedBy: "$addedBy",
+    //           updatedBy: "$updatedBy",
+    //           updatedAt: "$updatedAt",
+    //         },
+    //       },
+    //       {
+    //         $match: query,
+    //       },
+    //     ])
+    //     .toArray((err, totalResult) => {
+    //       db.collection("category")
+    //         .aggregate([
+    //           {
+    //             $project: {
+    //               id: "$_id",
+    //               isDeleted: "$isDeleted",
+    //               name: "$name",
+    //               description: "$description",
+    //               cat_type: "$cat_type",
+    //               image: "$image",
+    //               status: "$status",
+    //               createdAt: "$createdAt",
+    //               deletedBy: "$deletedBy.fullName",
+    //               deletedAt: "$deletedAt",
+    //               addedBy: "$addedBy",
+    //               updatedBy: "$updatedBy",
+    //               updatedAt: "$updatedAt",
+    //             },
+    //           },
+    //           {
+    //             $match: query,
+    //           },
+    //           {
+    //             $sort: sortquery,
+    //           },
 
-//           console.log(query)
-//           db.collection('category').aggregate([
-              
-//               {
-//                   $project: {
-//                       id: "$_id",
-//                       isDeleted: "$isDeleted", 
-//                       name: "$name",
-//                       description:"$description",
-//                       cat_type:"$cat_type",
-//                       image:"$image",
-//                       status: "$status",
-//                       createdAt: "$createdAt",
-//                       deletedBy: "$deletedBy.fullName",
-//                       deletedAt: '$deletedAt',
-//                       addedBy: "$addedBy",
-//                       updatedBy: "$updatedBy", 
-//                       updatedAt:"$updatedAt",
-
-//                   }
-//               },
-//               {
-//                   $match: query
-//               },
-//           ]).toArray((err, totalResult) => {
-
-//               db.collection('category').aggregate([
-                 
-
-//                 {
-//                   $project: {
-//                       id: "$_id",
-//                       isDeleted: "$isDeleted", 
-//                       name: "$name",
-//                       description:"$description",
-//                       cat_type:"$cat_type",
-//                       image:"$image",
-//                       status: "$status",
-//                       createdAt: "$createdAt",
-//                       deletedBy: "$deletedBy.fullName",
-//                       deletedAt: '$deletedAt',
-//                       addedBy: "$addedBy",
-//                       updatedBy: "$updatedBy", 
-//                       updatedAt:"$updatedAt",
-
-//                   }
-//                 },
-//                   {
-//                       $match: query
-//                   },
-//                   {
-//                       $sort: sortquery
-//                   },
-
-//                   {
-//                       $skip: Number(skipNo)
-//                   },
-//                   {
-//                       $limit: Number(count)
-//                   }
-//               ]).toArray(async (err, result) => {
-                  
-//                   let resData = {
-//                       "success": true,
-//                       "data": result,
-//                       "total": totalResult.length,
-//                   }
-//                   if (!req.param('count') && !req.param('page')) {
-//                       resData.data = result ? result : []
-//                   }
-//                   return res.status(200).json(resData);
-//               })
-
-//           })
-//       } catch (err) {
-//           console.log(err)
-//           return res.status(400).json({
-//               success: false,
-//               error: { code: 400, message: "" + err }
-//           })
-
-//       }
-//   },
+    //           {
+    //             $skip: Number(skipNo),
+    //           },
+    //           {
+    //             $limit: Number(count),
+    //           },
+    //         ])
+    //         .toArray(async (err, result) => {
+    //           let resData = {
+    //             success: true,
+    //             data: result,
+    //             total: totalResult.length,
+    //           };
+    //           if (!req.param("count") && !req.param("page")) {
+    //             resData.data = result ? result : [];
+    //           }
+    //           return res.status(200).json(resData);
+    //         });
+    //     });
+    } catch (err) {
+      console.log(err);
+      return res.status(400).json({
+        success: false,
+        error: { code: 400, message: "" + err },
+      });
+    }
+  },
 
   /*For edit category*/
 
   editCategory: async (req, res) => {
-      try {
-          // const id = req.param("id");
-          const id = req.body.id;
-          const data = req.body
+    try {
+      // const id = req.param("id");
+      const id = req.body.id;
+      const data = req.body;
 
-          if (!id) {
-              return res.status(404).json({
-                  success: false,
-                  error: { code: 400, message: constantObj.CATEGORY.ID_REQUIRED }
-              })
-          }
-
-          if (!data.name || typeof data.name === undefined) {
-              return res.status(404).json({
-                  success: false,
-                  error: { code: 404, message: constantObj.CATEGORY.NAME_REQUIRED }
-              })
-          }
-          const query = {};
-          query.isDeleted = false
-          query.name = data.name;
-          query.id = { "!=": id };
-
-          var existed = await Category.findOne(query);
-          if (existed) {
-              return res.status(400).json({
-                  success: false,
-                  error: { code: 400, message: constantObj.CATEGORY.NAME_EXIST }
-              })
-          }
-
-          data.updatedBy = req.identity.id;
-          let updatedCategory = await Category.updateOne({ id: id }, req.body);
-          return res.status(200).json({
-              success: true,
-              code: 200,
-              message: constantObj.CATEGORY.UPDATED
-          });
-      } catch (err) {
-          // console.log(err, '==========err');
-          return res.status(400).json({
-              success: false,
-              error: { code: 400, message: "" + err }
-          })
+      if (!id) {
+        return res.status(404).json({
+          success: false,
+          error: { code: 400, message: constantObj.CATEGORY.ID_REQUIRED },
+        });
       }
+
+      if (!data.name || typeof data.name === undefined) {
+        return res.status(404).json({
+          success: false,
+          error: { code: 404, message: constantObj.CATEGORY.NAME_REQUIRED },
+        });
+      }
+      const query = {};
+      query.isDeleted = false;
+      query.name = data.name;
+      query.id = { "!=": id };
+
+      var existed = await Category.findOne(query);
+      if (existed) {
+        return res.status(400).json({
+          success: false,
+          error: { code: 400, message: constantObj.CATEGORY.NAME_EXIST },
+        });
+      }
+
+      data.updatedBy = req.identity.id;
+      let updatedCategory = await Category.updateOne({ id: id }, req.body);
+      return res.status(200).json({
+        success: true,
+        code: 200,
+        message: constantObj.CATEGORY.UPDATED,
+      });
+    } catch (err) {
+      // console.log(err, '==========err');
+      return res.status(400).json({
+        success: false,
+        error: { code: 400, message: "" + err },
+      });
+    }
   },
 
   /*For delete category*/
 
   deleteCategory: async (req, res) => {
-      try {
-          const id = req.param("id");
+    try {
+      const id = req.param("id");
 
-          if (!id) {
-              return res.status(404).json({
-                  success: false,
-                  error: { code: 400, message: constantObj.CATEGORY.ID_REQUIRED }
-              })
-          } else {
-              let category = await Category.findOne({ id: id });
-              if (!category) {
-                  return res.status(400).json({
-                      success: false,
-                      error: { code: 400, message: constantObj.CATEGORY.INVALID_ID }
-                  })
-              } else {
-                  let removeCategory = await Category.updateOne({ id: id }, { isDeleted: true });
-                  return res.status(200).json({
-                      success: true,
-                      message: constantObj.CATEGORY.DELETED
-                  })
-              }
-          }
-      } catch (err) {
-          //(err);
+      if (!id) {
+        return res.status(404).json({
+          success: false,
+          error: { code: 400, message: constantObj.CATEGORY.ID_REQUIRED },
+        });
+      } else {
+        let category = await Category.findOne({ id: id });
+        if (!category) {
           return res.status(400).json({
-              success: false,
-              error: { code: 400, message: "" + err }
-          })
+            success: false,
+            error: { code: 400, message: constantObj.CATEGORY.INVALID_ID },
+          });
+        } else {
+          let removeCategory = await Category.updateOne(
+            { id: id },
+            { isDeleted: true }
+          );
+          return res.status(200).json({
+            success: true,
+            message: constantObj.CATEGORY.DELETED,
+          });
+        }
       }
+    } catch (err) {
+      //(err);
+      return res.status(400).json({
+        success: false,
+        error: { code: 400, message: "" + err },
+      });
+    }
   },
 
   /*For category Detail*/
 
-  categoryDetail: async (req, res,) => {
+  categoryDetail: async (req, res) => {
+    try {
+      const id = req.param("id");
 
-      try {
-          const id = req.param("id");
-
-          if (!id) {
-              return res.status(404).json({
-                  success: false,
-                  error: { code: 400, message: constantObj.user.PAYLOAD_MISSING }
-              })
-          } else {
-              const categoryDetail = await Category.findOne({ id: id }).populate('addedBy').populate('updatedBy')
-              // console.log(categoryDetail)
-              return res.status(200).json({
-                  success: true,
-                  data: categoryDetail,
-
-              })
-          }
-      } catch (err) {
-          //(err);
-          return res.status(400).json({
-              success: false,
-              error: { code: 400, message: "" + err }
-          })
+      if (!id) {
+        return res.status(404).json({
+          success: false,
+          error: { code: 400, message: constantObj.user.PAYLOAD_MISSING },
+        });
+      } else {
+        const categoryDetail = await Category.findOne({ id: id })
+          .populate("addedBy")
+          .populate("updatedBy");
+        // console.log(categoryDetail)
+        return res.status(200).json({
+          success: true,
+          data: categoryDetail,
+        });
       }
+    } catch (err) {
+      //(err);
+      return res.status(400).json({
+        success: false,
+        error: { code: 400, message: "" + err },
+      });
+    }
   },
-
-
-}
+};
