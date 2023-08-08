632645a9b5fb7ae7ebca6f8be030a9a9279069ad
rishita <rishitarana@jcsoftwaresolution.com>
2023-08-08 18:55:05 +0530

    changes

--- api/controllers/ZoomController.js
+++ api/controllers/ZoomController.js
@@ -9,6 +9,17 @@ authenticate = function (req, res) {
     const authUrl = `https://zoom.us/oauth/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}`;
     res.redirect(authUrl);
 }
+generateOTP = function () {
+    // action are perform to generate VeificationCode for user
+    var length = 12,
+      charset = '1234567890',
+      retVal = '';
+  
+    for (var i = 0, n = charset.length; i < length; ++i) {
+      retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+  };
 
 // https://www.npmjs.com/package/jsrsasign
 
@@ -51,11 +62,11 @@ module.exports = {
 
     generateSignature: async function (req, res) {
         try {
-            const meetingNumber = new Date().getTime()
+            const meetingNumber = generateOTP()
             const apiKey = constant.ZOOM_MEETING_SDK_KEY_OR_CLIENT_ID;
             const apiSecret = constant.ZOOM_MEETING_SDK_SECRET_OR_CLIENT_SECRET;
             const timestamp = new Date().getTime() - 30000; // 30 seconds before to account for latency
-            console.log(timestamp, "==============timestamp")
+            // console.log(timestamp, "==============timestamp")
             const msg = Buffer.from(apiKey + meetingNumber + timestamp + '0').toString('base64');
             const hash = crypto.createHmac('sha256', apiSecret).update(msg).digest('base64');
             const signature = Buffer.from(`${apiKey}.${meetingNumber}.${timestamp}.0.${hash}`).toString('base64');
