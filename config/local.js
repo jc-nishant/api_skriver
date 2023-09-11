3b33a894fab82362e2fea957b5e30b75a9dafeda
rishita <rishitarana@jcsoftwaresolution.com>
2023-09-11 12:49:20 +0530

    changes

---
+++ config/local.js
@@ -0,0 +1,29 @@
+
+/**
+ * Local environment settings
+ *
+ * Use this file to specify configuration settings for use while developing
+ * the app on your personal system.
+ *
+ * For more information, check out:
+ * https://sailsjs.com/docs/concepts/configuration/the-local-js-file
+ */
+
+module.exports = {
+    port: process.env.PORT || 6037,
+    BACK_WEB_URL:'https://skriverapi.jcsoftwaresolution.in/',
+    FRONT_WEB_URL:'https://skriver-frontend.pages.dev/',
+    ADMIN_WEB_URL:'https://skriver-admin.pages.dev/',
+    DB_USER: 'skriver',
+    DB_PASSWORD: 'skriver1234',
+    DB_HOST: '74.208.206.18',
+    DB_PORT: 5432,
+    DB_NAME: 'skriver',
+    GOOGLE_API_KEY: 'AIzaSyDmvz3A-BAjR77dy4PsaoHJC15mUdffLSA',
+    ZOOM_MEETING_SDK_KEY_OR_CLIENT_ID: "cuqUzwYJT66ZXG1aJTqIjg",
+    ZOOM_MEETING_SDK_SECRET_OR_CLIENT_SECRET: "N09S1FUHby8TXwlKVVlSg7LyE8O3Go45",
+    PAYMENT_INFO: {
+      SECREATKEY: 'sk_test_BewEVDCCPMHmjtAYVVMhBWg4001ZGJWSbW',
+    },
+  
+  };
